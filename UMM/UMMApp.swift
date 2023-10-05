89859b2874d7c6c1f40b09ff5dbcc9323f3105dc
Oliver Wonil <stellaria700@gmail.com>
2023-10-05 14:50:51 +0900

    :chore:[#3] 프로젝트 파일 추가

---
+++ UMM/UMMApp.swift
@@ -0,0 +1,17 @@
+//
+//  UMMApp.swift
+//  UMM
+//
+//  Created by Wonil Lee on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct UMMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
