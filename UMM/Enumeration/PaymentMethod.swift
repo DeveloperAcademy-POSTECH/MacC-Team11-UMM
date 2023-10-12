d5da0dce3d1356636014a299a8b166b7a2f9475c
Oliver Wonil <stellaria700@gmail.com>
2023-10-12 16:26:30 +0900

    :zap:[#11] RecordViewModel에 divideVoiceSentence() 추가 - 미완성

---
+++ UMM/Enumeration/PaymentMethod.swift
@@ -0,0 +1,14 @@
+//
+//  PaymentMethod.swift
+//  UMM
+//
+//  Created by Wonil Lee on 10/12/23.
+//
+
+import Foundation
+
+enum PaymentMethod: Int {
+    case unknown = 0
+    case card
+    case cash
+}
