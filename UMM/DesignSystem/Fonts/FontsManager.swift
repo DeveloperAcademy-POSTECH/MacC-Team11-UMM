aa1c345b25990194f71e7ce38c4ce97291bf581d
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-10-16 21:52:54 +0900

    🧩[#38] Pretendard 추가

---
+++ UMM/DesignSystem/Fonts/FontsManager.swift
@@ -0,0 +1,22 @@
+//
+//  FontsManager.swift
+//  UMM
+//
+//  Created by GYURI PARK on 2023/10/16.
+//
+
+import Foundation
+
+struct FontsManager {
+    struct Pretendard {
+        static let black = "Pretendard-Black"
+        static let bold = "Pretendard-Bold"
+        static let extraBold = "Pretendard-ExtraBold"
+        static let extraLight = "Pretendard-ExtraLight"
+        static let light = "Pretendard-Light"
+        static let medium = "Pretendard-Medium"
+        static let regular = "Pretendard-Regular"
+        static let semiBold = "Pretendard-SemiBold"
+        static let thin = "Pretendard-Thin"
+    }
+}
