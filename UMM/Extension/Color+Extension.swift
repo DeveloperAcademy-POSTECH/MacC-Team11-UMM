81349894cdc38d6d5a3b0eb7973a6f1e4e058ae3
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-10-16 22:19:14 +0900

    🧩[#38] ColorSet 추가

---
+++ UMM/Extension/Color+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Color+Extension.swift
+//  UMM
+//
+//  Created by GYURI PARK on 2023/10/16.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    init(_ hex: UInt, alpha: Double = 1){
+        self.init(
+            .sRGB,
+        red: Double((hex>>16) & 0xFF) / 255,
+            green: Double((hex >> 8) & 0xFF) / 255,
+        blue: Double((hex >> 0) & 0xFF) / 255,
+        opacity: alpha
+        )
+    }
+}
