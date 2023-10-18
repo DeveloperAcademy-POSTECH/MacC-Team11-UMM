69dc555d6970432bec9ce907e1c38e996ef84889
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-10-18 15:42:46 +0900

    🧩[#57] Font+Extension 추가

--- UMM/DesignSystem/Fonts/FontsManager.swift
+++ UMM/DesignSystem/Fonts/FontsManager.swift
@@ -21,22 +21,3 @@ struct FontsManager {
         static let thin = "Pretendard-Thin"
     }
 }
-
-let display1 = Font.custom(FontsManager.Pretendard.semiBold, size: 20)
-let display2 = Font.custom(FontsManager.Pretendard.semiBold, size: 24)
-let display3 = Font.custom(FontsManager.Pretendard.semiBold, size: 28)
-
-let subhead1 = Font.custom(FontsManager.Pretendard.semiBold, size: 14)
-let subhead2_1 = Font.custom(FontsManager.Pretendard.semiBold, size: 16)
-let subhead2_2 = Font.custom(FontsManager.Pretendard.medium, size: 16)
-let subhead3_1 = Font.custom(FontsManager.Pretendard.semiBold, size: 18)
-let subhead3_2 = Font.custom(FontsManager.Pretendard.medium, size: 18)
-
-let body2 = Font.custom(FontsManager.Pretendard.medium, size: 16)
-let body3 = Font.custom(FontsManager.Pretendard.medium, size: 18)
-
-let caption1 = Font.custom(FontsManager.Pretendard.medium, size: 12)
-let caption2 = Font.custom(FontsManager.Pretendard.medium, size: 14)
-
-let calendar1 = Font.custom(FontsManager.Pretendard.medium, size: 20)
-let calendar2 = Font.custom(FontsManager.Pretendard.regular, size: 20)
---
+++ UMM/Extension/Font+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  Font+Extension.swift
+//  UMM
+//
+//  Created by GYURI PARK on 2023/10/18.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static let display1 = Font.custom(FontsManager.Pretendard.semiBold, size: 20)
+    static let display2 = Font.custom(FontsManager.Pretendard.semiBold, size: 24)
+    static let display3 = Font.custom(FontsManager.Pretendard.semiBold, size: 28)
+    
+    static let subhead1 = Font.custom(FontsManager.Pretendard.semiBold, size: 14)
+    static let subhead2_1 = Font.custom(FontsManager.Pretendard.semiBold, size: 16)
+    static let subhead2_2 = Font.custom(FontsManager.Pretendard.medium, size: 16)
+    static let subhead3_1 = Font.custom(FontsManager.Pretendard.semiBold, size: 18)
+    static let subhead3_2 = Font.custom(FontsManager.Pretendard.medium, size: 18)
+    
+    static let body2 = Font.custom(FontsManager.Pretendard.medium, size: 16)
+    static let body3 = Font.custom(FontsManager.Pretendard.medium, size: 18)
+    
+    static let caption1 = Font.custom(FontsManager.Pretendard.medium, size: 12)
+    static let caption2 = Font.custom(FontsManager.Pretendard.medium, size: 14)
+    
+    static let calendar1 = Font.custom(FontsManager.Pretendard.medium, size: 20)
+    static let calendar2 = Font.custom(FontsManager.Pretendard.regular, size: 20)
+}
