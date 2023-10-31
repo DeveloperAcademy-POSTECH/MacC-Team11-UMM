c95b5368d83d26928c86d83c0007e642fe48a646
Oliver Wonil <stellaria700@gmail.com>
2023-10-31 11:01:13 +0900

    :art:[#132] 중간 저장

---
+++ UMM/View/Modal/CountryChoiceModal.swift
@@ -0,0 +1,18 @@
+//
+//  CountryChoiceModal.swift
+//  UMM
+//
+//  Created by Wonil Lee on 10/31/23.
+//
+
+import SwiftUI
+
+struct CountryChoiceModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountryChoiceModal()
+}
