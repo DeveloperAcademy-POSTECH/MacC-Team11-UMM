193c16474f1494e2b52f871170c7effa304db11e
Oliver Wonil <stellaria700@gmail.com>
2023-11-05 04:00:41 +0900

    zap:[#159] convert 메서드에서는 addingTimeInterval만 하도록 변경

--- UMM/Handler/DateGapHandler.swift
+++ UMM/Handler/DateGapHandler.swift
@@ -21,11 +21,37 @@ final class DateGapHandler {
     
     private init() {
         getLocation()
+        let baseCoordinate = CLLocationCoordinate2D(latitude: 37.56, longitude: 127.00) // 서울 (latitude: 37.56, longitude: 127.00)
+        // CLGeocoder를 사용하여 위치 정보를 가져온다.
+        let geocoder = CLGeocoder()
+        let nowDate = Date()
+        
+        geocoder.reverseGeocodeLocation(CLLocation(latitude: baseCoordinate.latitude, longitude: baseCoordinate.longitude)) { (placemarks, _) in
+            if let basePlacemark = placemarks?.first {
+                if let currentLocation = self.currentLocation {
+                    geocoder.reverseGeocodeLocation(currentLocation) { (placemarks, _) in
+                        if let currentPlacemark = placemarks?.first {
+                            // 현재 위치와 목표 위치의 TimeZone을 가져온다.
+                            if let baseTimeZone = basePlacemark.timeZone, let currentTimeZone = currentPlacemark.timeZone {
+                                self.timeDifferenceInterval = TimeInterval(currentTimeZone.secondsFromGMT(for: nowDate) - baseTimeZone.secondsFromGMT(for: nowDate))
+                            } else {
+                                print("DateGapHandler | Failed to fetch time zones.")
+                            }
+                        } else {
+                            print("DateGapHandler | Failed to fetch current placemark.")
+                        }
+                    }
+                }
+            } else {
+                print("DateGapHandler | Failed to fetch base placemark.")
+            }
+        }
     }
     
     private var locationManager: CLLocationManager?
     private var locationManagerDelegate = LocationManagerDelegateForDateGapHandler()
     var currentLocation: CLLocation?
+    var timeDifferenceInterval: TimeInterval = 0
     
     private func getLocation() {
         let locationManager = CLLocationManager()
@@ -42,66 +68,12 @@ final class DateGapHandler {
     }
     
     func convertBeforeSaving(date: Date) -> Date {
-        let baseCoordinate = CLLocationCoordinate2D(latitude: 37.56, longitude: 127.00) // 서울 (latitude: 37.56, longitude: 127.00)
-        // CLGeocoder를 사용하여 위치 정보를 가져온다.
-        let geocoder = CLGeocoder()
-        let nowDate = Date()
-        var tbr: Date = date
-        
-        geocoder.reverseGeocodeLocation(CLLocation(latitude: baseCoordinate.latitude, longitude: baseCoordinate.longitude)) { (placemarks, _) in
-            if let basePlacemark = placemarks?.first {
-                if let currentLocation = self.currentLocation {
-                    geocoder.reverseGeocodeLocation(currentLocation) { (placemarks, _) in
-                        if let currentPlacemark = placemarks?.first {
-                            // 현재 위치와 목표 위치의 TimeZone을 가져온다.
-                            if let baseTimeZone = basePlacemark.timeZone, let currentTimeZone = currentPlacemark.timeZone {
-                                let timeDifference = currentTimeZone.secondsFromGMT(for: nowDate) - baseTimeZone.secondsFromGMT(for: nowDate)
-                                // base가 3시일 때 current가 5시이면 timeDifference는 7200. 저장하기 전에 date에 7200초를 더하면 base 기준 5시인 date가 된다.
-                                tbr = date.addingTimeInterval(TimeInterval(timeDifference))
-                            } else {
-                                print("Failed to fetch time zones.")
-                            }
-                        } else {
-                            print("Failed to fetch current placemark.")
-                        }
-                    }
-                }
-            } else {
-                print("Failed to fetch base placemark.")
-            }
-        }
-        return tbr
+        // base가 3시일 때 current가 5시이면 timeDifference는 7200. 저장하기 전에 date에 7200초를 더하면 base 기준 5시인 date가 된다.
+        return date.addingTimeInterval(timeDifferenceInterval)
     }
     
     func convertBeforeShowing(date: Date) -> Date {
-        let baseCoordinate = CLLocationCoordinate2D(latitude: 37.56, longitude: 127.00) // 서울 (latitude: 37.56, longitude: 127.00)
-        // CLGeocoder를 사용하여 위치 정보를 가져온다.
-        let geocoder = CLGeocoder()
-        let nowDate = Date()
-        var tbr: Date = date
-        
-        geocoder.reverseGeocodeLocation(CLLocation(latitude: baseCoordinate.latitude, longitude: baseCoordinate.longitude)) { (placemarks, _) in
-            if let basePlacemark = placemarks?.first {
-                if let currentLocation = self.currentLocation {
-                    geocoder.reverseGeocodeLocation(currentLocation) { (placemarks, _) in
-                        if let currentPlacemark = placemarks?.first {
-                            // 현재 위치와 목표 위치의 TimeZone을 가져온다.
-                            if let baseTimeZone = basePlacemark.timeZone, let currentTimeZone = currentPlacemark.timeZone {
-                                let timeDifference = currentTimeZone.secondsFromGMT(for: nowDate) - baseTimeZone.secondsFromGMT(for: nowDate)
-                                // base가 3시일 때 current가 5시이면 timeDifference는 7200. 저장된 date가 base 기준 1시라고 하자. 이것은 current 기준 3시. 보여주기 전에 date에서 7200초를 빼면 current 기준 1시인 date가 된다.
-                                tbr = date.addingTimeInterval(TimeInterval(-timeDifference))
-                            } else {
-                                print("Failed to fetch time zones.")
-                            }
-                        } else {
-                            print("Failed to fetch current placemark.")
-                        }
-                    }
-                }
-            } else {
-                print("Failed to fetch base placemark.")
-            }
-        }
-        return tbr
+        // base가 3시일 때 current가 5시이면 timeDifference는 7200. 저장된 date가 base 기준 1시라고 하자. 이것은 current 기준 3시. 보여주기 전에 date에서 7200초를 빼면 current 기준 1시인 date가 된다.
+        return date.addingTimeInterval(-timeDifferenceInterval)
     }
-}
+} 
