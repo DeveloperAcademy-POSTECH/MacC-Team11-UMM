6fec2f488efa981534c7ae59c6c34039566251e5
Oliver Wonil <stellaria700@gmail.com>
2023-11-06 21:24:00 +0900

    :broom:[#141] CountryInfo, CurrencyInfo 적용했다.

--- UMM/View/Record/CompleteRecordView.swift
+++ UMM/View/Record/CompleteRecordView.swift
@@ -4,37 +4,37 @@
 //
 //  Created by Wonil Lee on 10/16/23.
 //
-
-import SwiftUI
-
-struct CompleteRecordView: View {
-    @ObservedObject var viewModel: RecordViewModel
-    
-    var body: some View {
-        VStack(spacing: 60) {
-            Button {
-                if let fileName = viewModel.soundRecordFileName {
+//
+//import SwiftUI
+//
+//struct CompleteRecordView: View {
+//    @ObservedObject var viewModel: RecordViewModel
+//    
+//    var body: some View {
+//        VStack(spacing: 60) {
+//            Button {
+//                if let fileName = viewModel.soundRecordFileName {
 //                    viewModel.startPlayingAudio(url: fileName)
-                }
-            } label: {
-                Text("Play Audio")
-                    .font(.title2)
-            }
-            .buttonStyle(.bordered)
-            
-            Button {
-                if let fileName = viewModel.soundRecordFileName {
+//                }
+//            } label: {
+//                Text("Play Audio")
+//                    .font(.title2)
+//            }
+//            .buttonStyle(.bordered)
+//            
+//            Button {
+//                if let fileName = viewModel.soundRecordFileName {
 //                    viewModel.stopPlayingAudio()
-                }
-            } label: {
-                Text("Stop Audio")
-                    .font(.title2)
-            }
-            .buttonStyle(.bordered)
-        }
-    }
-}
-
-#Preview {
-    CompleteRecordView(viewModel: RecordViewModel())
-}
+//                }
+//            } label: {
+//                Text("Stop Audio")
+//                    .font(.title2)
+//            }
+//            .buttonStyle(.bordered)
+//        }
+//    }
+//}
+//
+//#Preview {
+//    CompleteRecordView(viewModel: RecordViewModel())
+//}
