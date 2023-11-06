f705845be1265bde9854e877f35da529aef57385
Oliver Wonil <stellaria700@gmail.com>
2023-11-06 22:47:30 +0900

    체크

--- UMM/View/Record/CompleteRecordView.swift
+++ UMM/View/Record/CompleteRecordView.swift
@@ -1,40 +1,40 @@
 //
-//  CompleteRecordView.swift
-//  UMM
+//      CompleteRecordView.swift
+//      UMM
 //
-//  Created by Wonil Lee on 10/16/23.
+//      Created by Wonil Lee on 10/16/23.
 //
 //
-//import SwiftUI
+//    import SwiftUI
 //
-//struct CompleteRecordView: View {
-//    @ObservedObject var viewModel: RecordViewModel
-//    
-//    var body: some View {
-//        VStack(spacing: 60) {
-//            Button {
-//                if let fileName = viewModel.soundRecordFileName {
-//                    viewModel.startPlayingAudio(url: fileName)
+//    struct CompleteRecordView: View {
+//        @ObservedObject var viewModel: RecordViewModel
+//        
+//        var body: some View {
+//            VStack(spacing: 60) {
+//                Button {
+//                    if let fileName = viewModel.soundRecordFileName {
+//                        viewModel.startPlayingAudio(url: fileName)
+//                    }
+//                } label: {
+//                    Text("Play Audio")
+//                        .font(.title2)
 //                }
-//            } label: {
-//                Text("Play Audio")
-//                    .font(.title2)
-//            }
-//            .buttonStyle(.bordered)
-//            
-//            Button {
-//                if let fileName = viewModel.soundRecordFileName {
-//                    viewModel.stopPlayingAudio()
+//                .buttonStyle(.bordered)
+//                
+//                Button {
+//                    if let fileName = viewModel.soundRecordFileName {
+//                        viewModel.stopPlayingAudio()
+//                    }
+//                } label: {
+//                    Text("Stop Audio")
+//                        .font(.title2)
 //                }
-//            } label: {
-//                Text("Stop Audio")
-//                    .font(.title2)
+//                .buttonStyle(.bordered)
 //            }
-//            .buttonStyle(.bordered)
 //        }
 //    }
-//}
 //
-//#Preview {
-//    CompleteRecordView(viewModel: RecordViewModel())
-//}
+//    #Preview {
+//        CompleteRecordView(viewModel: RecordViewModel())
+//    }
