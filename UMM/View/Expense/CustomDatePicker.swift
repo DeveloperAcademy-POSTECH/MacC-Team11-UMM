64d8f98f1ee909e808e67f0671f682e18fca3718
kimpepe <unboxing96@gmail.com>
2023-11-22 11:50:39 +0900

    :art:[#323] customDatePicker를 기본 datePicker로 변경하기

--- UMM/View/Expense/CustomDatePicker.swift
+++ UMM/View/Expense/CustomDatePicker.swift
@@ -117,9 +117,8 @@ struct CustomDatePicker: View {
     let dateGapHandler = DateGapHandler.shared
     
     var body: some View {
-        let endDateOfTravel = expenseViewModel.datePickerRange().upperBound
         
-        HStack(spacing: 0) {
+        HStack(spacing: 2) {
             // selectedDate == startDateOfTravel이면 inActiveLeft
             // 나머지 경우에는 activeLeft
             Button(action: {
@@ -130,29 +129,41 @@ struct CustomDatePicker: View {
                     .scaledToFit()
                     .frame(width: 16, height: 16)
             })
-
-            ZStack {
-                Button {
-                    expenseViewModel.triggerDatePickerPopover(pickerId: pickerId)
-                } label: {
-                    Text("\(expenseViewModel.selectedDate, formatter: dateFormatterWithDay)")
-                        .foregroundStyle(.black)
-                }
-                
-                // 안 보이게 하고 Button으로 호출
-                DatePicker("", selection: $expenseViewModel.selectedDate,
-                           displayedComponents: [.date])
-                    .labelsHidden()
-                    .accessibilityIdentifier(pickerId)
-                    .onReceive(expenseViewModel.$selectedDate) { _ in
-                        DispatchQueue.main.async {
-                            expenseViewModel.filteredTodayExpenses = expenseViewModel.getFilteredTodayExpenses()
-                            expenseViewModel.groupedTodayExpenses = Dictionary(grouping: expenseViewModel.filteredTodayExpenses, by: { $0.country })
-                        }
+            
+              // MARK: - 커스텀 datePicker
+//            ZStack {
+//                Button {
+//                    expenseViewModel.triggerDatePickerPopover(pickerId: pickerId)
+//                } label: {
+//                    Text("\(expenseViewModel.selectedDate, formatter: dateFormatterWithDay)")
+//                        .foregroundStyle(.black)
+//                }
+//                
+//                // 안 보이게 하고 Button으로 호출
+//                DatePicker("", selection: $expenseViewModel.selectedDate,
+//                           displayedComponents: [.date])
+//                    .labelsHidden()
+//                    .accessibilityIdentifier(pickerId)
+//                    .onReceive(expenseViewModel.$selectedDate) { _ in
+//                        DispatchQueue.main.async {
+//                            expenseViewModel.filteredTodayExpenses = expenseViewModel.getFilteredTodayExpenses()
+//                            expenseViewModel.groupedTodayExpenses = Dictionary(grouping: expenseViewModel.filteredTodayExpenses, by: { $0.country })
+//                        }
+//                    }
+//                    .colorInvert()
+//                    .colorMultiply(Color.clear)
+//            }
+            
+            // MARK: - 기본 datePicker
+            DatePicker("", selection: $expenseViewModel.selectedDate, displayedComponents: [.date])
+                .labelsHidden()
+                .onReceive(expenseViewModel.$selectedDate) { _ in
+                    DispatchQueue.main.async {
+                        expenseViewModel.filteredTodayExpenses = expenseViewModel.getFilteredTodayExpenses()
+                        expenseViewModel.groupedTodayExpenses = Dictionary(grouping: expenseViewModel.filteredTodayExpenses, by: { $0.country })
                     }
-                    .colorInvert()
-                    .colorMultiply(Color.clear)
-            }
+                }
+            
             Button(action: {
                 selectedDate = selectedDate.addingTimeInterval(86400)
             }, label: {
